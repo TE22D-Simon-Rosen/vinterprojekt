fecaa84e839f7fec5938105ece40a991922d5866
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2025-01-28 10:11:47 +0100

    first commit

---
+++ vinterprojekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Json;
+HttpClient client = new();
+client.BaseAddress = new("https://random-word-api.herokuapp.com/"); //Bas API
+
+List<string> words = new();
+
+HttpResponseMessage response = client.GetAsync("word?length=5").Result; //Hämtar ett random ord på 5 bokstäver
+
+words = response.Content.ReadFromJsonAsync<List<string>>().Result;
+
+File.WriteAllText("word.json", response.Content.ReadAsStringAsync().Result);
+
+Console.WriteLine(words[0]);
+
+
+Console.ReadLine();
